8334f3f030ff7ee8e23e8f5b78fa7af4adc04769
Lars Tode <lars.tode@dkd.de>
2024-06-07 11:42:14 +0200

    [TASK] Switch typoscript file extension
    This commit changes the file extension for TypoScript from txt to typoscript.
    
    This change is done according to the coding guide lines.
    
    https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/CodingGuidelines/CglTypoScript/Index.html
    
    Refs: #17

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,12 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:static_info_tables_zh/Configuration/TypoScript/Extbase/setup.txt">'
-);
+call_user_func(function(string $extensionKey)
+{
+    ExtensionManagementUtility::addTypoScriptSetup(
+        '@import "EXT:static_info_tables_zh/Configuration/TypoScript/Extbase/setup.typoscript"'
+    );
+}, 'static_info_tables_zh');
